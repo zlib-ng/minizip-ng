b2c7e7d3c1ccf1f377ff0daf89e1a436f5f15de1
Nathan Moinvaziri <nathan@nathanm.com>
2012-10-25 02:20:34 -0700

    not going to support this

--- mztools.c
+++ mztools.c
@@ -8,8 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "zlib.h"
-#include "unzip.h"
+//#include "unzip.h"
 
 #define READ_8(adr)  ((unsigned char)*(adr))
 #define READ_16(adr) ( READ_8(adr) | (READ_8(adr+1) << 8) )
