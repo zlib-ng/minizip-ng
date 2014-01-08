8e60f1fbf997453469b40fca5887f70da9342040
Nathan Moinvaziri <nathan@nathanm.com>
2014-01-07 18:58:43 -0700

    Don't auto-detect Windows RT

--- iowin32.c
+++ iowin32.c
@@ -23,11 +23,9 @@
 #define INVALID_SET_FILE_POINTER ((DWORD)-1)
 #endif
 
-#if defined(WINAPI_FAMILY_PARTITION) && (!(defined(IOWIN32_USING_WINRT_API)))
-  #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
-    #define IOWIN32_USING_WINRT_API 1
-  #endif
-#endif
+/*#ifndef IOWIN32_USING_WINRT_API
+    #define IOWIN32_USING_WINRT_API
+#endif*/
 
 voidpf  ZCALLBACK win32_open_file_func  OF((voidpf opaque, const char* filename, int mode));
 uLong   ZCALLBACK win32_read_file_func  OF((voidpf opaque, voidpf stream, void* buf, uLong size));
