103d74f7fd1032b8aef7cf1d8287bd41587e7b38
Andy Maloney <asmaloney@gmail.com>
2017-08-23 19:59:16 -0400

    Move declarations to avoid unused warnings when some #ifdefs are undefined

--- unzip.c
+++ unzip.c
@@ -1333,7 +1333,6 @@ extern int ZEXPORT unzReadCurrentFile(unzFile file, voidp buf, uint32_t len)
             uint32_t bytes_not_read = 0;
             uint32_t bytes_read = 0;
             uint32_t total_bytes_read = 0;
-            uint32_t i = 0;
 
             if (s->pfile_in_zip_read->stream.next_in != NULL)
                 bytes_not_read = (uint32_t)(s->pfile_in_zip_read->read_buffer + UNZ_BUFSIZE -
@@ -1384,6 +1383,8 @@ extern int ZEXPORT unzReadCurrentFile(unzFile file, voidp buf, uint32_t len)
 #endif
                 if (s->pcrc_32_tab != NULL)
                 {
+                    uint32_t i = 0;
+
                     for (i = 0; i < total_bytes_read; i++)
                       s->pfile_in_zip_read->read_buffer[i] =
                           zdecode(s->keys, s->pcrc_32_tab, s->pfile_in_zip_read->read_buffer[i]);
