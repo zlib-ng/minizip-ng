fdc695be86b55d2568115a215534c2ef4e8b9730
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2017-10-26 10:21:21 -0700

    Added zip memory test.

--- test/test.h
+++ test/test.h
@@ -5,11 +5,16 @@
 extern "C" {
 #endif
 
+/***************************************************************************/
+
 void test_aes();
 void test_crypt();
 void test_inflate();
 void test_deflate();
 void test_bzip();
+void test_zip_mem();
+
+/***************************************************************************/
 
 #ifdef __cplusplus
 }
