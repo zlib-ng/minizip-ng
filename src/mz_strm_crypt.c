82df3464975ad22341d1b17bb448016b79350bc7
Joshua Hudson <joshua@hudsons.io>
2018-02-21 17:18:09 -0800

    Update mz_strm_crypt.c
    fix Unknown type name 'z_crc_t'

--- src/mz_strm_crypt.c
+++ src/mz_strm_crypt.c
@@ -38,6 +38,10 @@
 
 #define RAND_HEAD_LEN  12
 
+#if ZLIB_VERNUM < 0x1270 //define z_crc_t in zlib 1.2.5 and less
+typedef unsigned long z_crc_t;
+#endif
+
 /***************************************************************************/
 
 mz_stream_vtbl mz_stream_crypt_vtbl = {
