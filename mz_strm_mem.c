ad8e3dd4b135b45f15472108fbcf09f824b845ab
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2018-04-18 20:54:52 -0700

    Version 2.2.9.

--- mz_strm_mem.c
+++ mz_strm_mem.c
@@ -1,5 +1,5 @@
 /* mz_strm_mem.c -- Stream for memory access
-   Version 2.2.8, March 15th, 2018
+   Version 2.2.9, April 18th, 2018
    part of the MiniZip project
 
    This interface is designed to access memory rather than files.
--- mz_strm_win32.c
+++ mz_strm_win32.c
@@ -1,5 +1,5 @@
 /* mz_strm_win32.c -- Stream for filesystem access for windows
-   Version 2.2.8, March 15th, 2018
+   Version 2.2.9, April 18th, 2018
    part of the MiniZip project
 
    Copyright (C) 2010-2018 Nathan Moinvaziri
