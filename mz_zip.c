eb80cd9fa4f23a890160ed5b218e7cfe4b4a2b77
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2018-07-11 16:45:09 -0700

    Fixed bug extracting 0-byte file when stored.

--- mz_zip.c
+++ mz_zip.c
@@ -1349,6 +1349,9 @@ extern int32_t mz_zip_entry_read(void *handle, void *buf, uint32_t len)
     if (len == 0)
         return MZ_PARAM_ERROR;
 
+    if (zip->file_info.compressed_size == 0)
+        return 0;
+
     // Read entire entry even if uncompressed_size = 0, otherwise
     // aes encryption validation will fail if compressed_size > 0
     read = mz_stream_read(zip->crc32_stream, buf, len);
