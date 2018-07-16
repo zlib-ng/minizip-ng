zlib-ng/minizip-ng

commit ea99a46b6d36a11bc3e3eb3a806482d68ab4a991

Define possibly missing #defines from zlib header. #264

mz_compat.h
@@ -22,17 +22,13 @@ extern "C" {
 
 #ifdef HAVE_ZLIB
 #include "zlib.h"
+#else
+#define ZEXPORT
+#define MAX_WBITS     (15)
+#define DEF_MEM_LEVEL (8)
 #endif
 
 /***************************************************************************/
-
-#ifndef DEF_MEM_LEVEL
-#  if MAX_MEM_LEVEL >= 8
-#    define DEF_MEM_LEVEL 8
-#  else
-#    define DEF_MEM_LEVEL  MAX_MEM_LEVEL
-#  endif
-#endif
 
 #if defined(USE_FILE32API)
 #  define MZ_USE_FILE32API
@@ -79,14 +75,6 @@ typedef struct tm tm_unz;
 typedef struct tm tm_zip;
 
 typedef uint64_t ZPOS64_T;
-
-#ifndef ZEXPORT
-#  ifdef _MSC_VER
-#    define ZEXPORT __declspec(dllexport)
-#  else
-#    define ZEXPORT
-#  endif
-#endif
 
 /***************************************************************************/
 

commit 7a0a62dc9c8b838b154b60872ce255da19e7cd99

restore DEF_MEM_LEVEL constant in mz_compat.h
This is defined in zutil.h in zlib, which is a private
header, so it doesn't get defined when #including zlib.h.
Instead if does get defined by minizip1 in zip.h, so
let's keep doing the same here as well.

Ref: https://github.com/madler/zlib/blob/v1.2.11/contrib/minizip/zip.h#L79-L85
Regression since: https://github.com/nmoinvaz/minizip/commit/ea99a46b6d36a11bc3e3eb3a806482d68ab4a991

mz_compat.h
@@ -22,6 +22,13 @@ extern "C" {
 
 #ifdef HAVE_ZLIB
 #include "zlib.h"
+#ifndef DEF_MEM_LEVEL
+#  if MAX_MEM_LEVEL >= 8
+#    define DEF_MEM_LEVEL 8
+#  else
+#    define DEF_MEM_LEVEL  MAX_MEM_LEVEL
+#  endif
+#endif
 #else
 #define ZEXPORT
 #define MAX_WBITS     (15)