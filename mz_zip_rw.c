d9a75b67806f8890ea337d129caa9eba5d063db0
Viktor Szakats <commit@vszakats.net>
2018-09-19 08:38:51 +0000

    mz_zip_reader_entry_read: remove const from buf parameter

--- mz_zip_rw.c
+++ mz_zip_rw.c
@@ -338,7 +338,7 @@ int32_t mz_zip_reader_entry_close(void *handle)
     return err;
 }
 
-int32_t mz_zip_reader_entry_read(void *handle, const void *buf, int32_t len)
+int32_t mz_zip_reader_entry_read(void *handle, void *buf, int32_t len)
 {
     mz_zip_reader *reader = (mz_zip_reader *)handle;
     int32_t read = 0;
