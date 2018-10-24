73025eb9db3558eeecda089f8a30282852edd7a2
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2018-10-24 09:23:14 -0700

    Renamed aes stream to wzaes for winzip aes. Removed sha1 stream.

--- mz_strm_wzaes.c
+++ mz_strm_wzaes.c
@@ -21,7 +21,7 @@
 #include "mz.h"
 #include "mz_os.h"
 #include "mz_strm.h"
-#include "mz_strm_aes.h"
+#include "mz_strm_wzaes.h"
 
 /***************************************************************************/
 
@@ -37,24 +37,24 @@
 
 /***************************************************************************/
 
-static mz_stream_vtbl mz_stream_aes_vtbl = {
-    mz_stream_aes_open,
-    mz_stream_aes_is_open,
-    mz_stream_aes_read,
-    mz_stream_aes_write,
-    mz_stream_aes_tell,
-    mz_stream_aes_seek,
-    mz_stream_aes_close,
-    mz_stream_aes_error,
-    mz_stream_aes_create,
-    mz_stream_aes_delete,
-    mz_stream_aes_get_prop_int64,
-    mz_stream_aes_set_prop_int64
+static mz_stream_vtbl mz_stream_wzaes_vtbl = {
+    mz_stream_wzaes_open,
+    mz_stream_wzaes_is_open,
+    mz_stream_wzaes_read,
+    mz_stream_wzaes_write,
+    mz_stream_wzaes_tell,
+    mz_stream_wzaes_seek,
+    mz_stream_wzaes_close,
+    mz_stream_wzaes_error,
+    mz_stream_wzaes_create,
+    mz_stream_wzaes_delete,
+    mz_stream_wzaes_get_prop_int64,
+    mz_stream_wzaes_set_prop_int64
 };
 
 /***************************************************************************/
 
-typedef struct mz_stream_aes_s {
+typedef struct mz_stream_wzaes_s {
     mz_stream       stream;
     int32_t         mode;
     int32_t         error;
@@ -70,13 +70,13 @@ typedef struct mz_stream_aes_s {
     uint8_t         nonce[AES_BLOCK_SIZE];
     uint8_t         encr_bfr[AES_BLOCK_SIZE];
     uint32_t        encr_pos;
-} mz_stream_aes;
+} mz_stream_wzaes;
 
 /***************************************************************************/
 
-int32_t mz_stream_aes_open(void *stream, const char *path, int32_t mode)
+int32_t mz_stream_wzaes_open(void *stream, const char *path, int32_t mode)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     uint16_t salt_length = 0;
     uint16_t password_length = 0;
     uint16_t key_length = 0;
@@ -172,17 +172,17 @@ int32_t mz_stream_aes_open(void *stream, const char *path, int32_t mode)
     return MZ_OK;
 }
 
-int32_t mz_stream_aes_is_open(void *stream)
+int32_t mz_stream_wzaes_is_open(void *stream)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     if (aes->initialized == 0)
         return MZ_STREAM_ERROR;
     return MZ_OK;
 }
 
-static int32_t mz_stream_aes_encrypt_data(void *stream, uint8_t *buf, int32_t size)
+static int32_t mz_stream_wzaes_encrypt_data(void *stream, uint8_t *buf, int32_t size)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     uint32_t pos = aes->encr_pos;
     uint32_t i = 0;
 
@@ -208,24 +208,24 @@ static int32_t mz_stream_aes_encrypt_data(void *stream, uint8_t *buf, int32_t si
     return MZ_OK;
 }
 
-int32_t mz_stream_aes_read(void *stream, void *buf, int32_t size)
+int32_t mz_stream_wzaes_read(void *stream, void *buf, int32_t size)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     int32_t read = 0;
     read = mz_stream_read(aes->stream.base, buf, size);
     if (read > 0)
     {
         hmac_sha_data((uint8_t *)buf, (uint32_t)read, aes->auth_ctx);
-        mz_stream_aes_encrypt_data(stream, (uint8_t *)buf, read);
+        mz_stream_wzaes_encrypt_data(stream, (uint8_t *)buf, read);
     }
 
     aes->total_in += read;
     return read;
 }
 
-int32_t mz_stream_aes_write(void *stream, const void *buf, int32_t size)
+int32_t mz_stream_wzaes_write(void *stream, const void *buf, int32_t size)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     int32_t written = 0;
 
     if (size < 0)
@@ -234,7 +234,7 @@ int32_t mz_stream_aes_write(void *stream, const void *buf, int32_t size)
         return MZ_STREAM_ERROR;
 
     memcpy(aes->buffer, buf, size);
-    mz_stream_aes_encrypt_data(stream, (uint8_t *)aes->buffer, size);
+    mz_stream_wzaes_encrypt_data(stream, (uint8_t *)aes->buffer, size);
     hmac_sha_data((uint8_t *)aes->buffer, (uint32_t)size, aes->auth_ctx);
 
     written = mz_stream_write(aes->stream.base, aes->buffer, size);
@@ -243,21 +243,21 @@ int32_t mz_stream_aes_write(void *stream, const void *buf, int32_t size)
     return written;
 }
 
-int64_t mz_stream_aes_tell(void *stream)
+int64_t mz_stream_wzaes_tell(void *stream)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     return mz_stream_tell(aes->stream.base);
 }
 
-int32_t mz_stream_aes_seek(void *stream, int64_t offset, int32_t origin)
+int32_t mz_stream_wzaes_seek(void *stream, int64_t offset, int32_t origin)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     return mz_stream_seek(aes->stream.base, offset, origin);
 }
 
-int32_t mz_stream_aes_close(void *stream)
+int32_t mz_stream_wzaes_close(void *stream)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     uint8_t authcode[MZ_AES_AUTHCODE_SIZE];
     uint8_t verify_authcode[MZ_AES_AUTHCODE_SIZE];
 
@@ -288,27 +288,27 @@ int32_t mz_stream_aes_close(void *stream)
     return MZ_OK;
 }
 
-int32_t mz_stream_aes_error(void *stream)
+int32_t mz_stream_wzaes_error(void *stream)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     return aes->error;
 }
 
-void mz_stream_aes_set_password(void *stream, const char *password)
+void mz_stream_wzaes_set_password(void *stream, const char *password)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     aes->password = password;
 }
 
-void mz_stream_aes_set_encryption_mode(void *stream, int16_t encryption_mode)
+void mz_stream_wzaes_set_encryption_mode(void *stream, int16_t encryption_mode)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     aes->encryption_mode = encryption_mode;
 }
 
-int32_t mz_stream_aes_get_prop_int64(void *stream, int32_t prop, int64_t *value)
+int32_t mz_stream_wzaes_get_prop_int64(void *stream, int32_t prop, int64_t *value)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     switch (prop)
     {
     case MZ_STREAM_PROP_TOTAL_IN:
@@ -332,9 +332,9 @@ int32_t mz_stream_aes_get_prop_int64(void *stream, int32_t prop, int64_t *value)
     return MZ_OK;
 }
 
-int32_t mz_stream_aes_set_prop_int64(void *stream, int32_t prop, int64_t value)
+int32_t mz_stream_wzaes_set_prop_int64(void *stream, int32_t prop, int64_t value)
 {
-    mz_stream_aes *aes = (mz_stream_aes *)stream;
+    mz_stream_wzaes *aes = (mz_stream_wzaes *)stream;
     switch (prop)
     {
     case MZ_STREAM_PROP_TOTAL_IN_MAX:
@@ -346,15 +346,15 @@ int32_t mz_stream_aes_set_prop_int64(void *stream, int32_t prop, int64_t value)
     return MZ_OK;
 }
 
-void *mz_stream_aes_create(void **stream)
+void *mz_stream_wzaes_create(void **stream)
 {
-    mz_stream_aes *aes = NULL;
+    mz_stream_wzaes *aes = NULL;
 
-    aes = (mz_stream_aes *)MZ_ALLOC(sizeof(mz_stream_aes));
+    aes = (mz_stream_wzaes *)MZ_ALLOC(sizeof(mz_stream_wzaes));
     if (aes != NULL)
     {
-        memset(aes, 0, sizeof(mz_stream_aes));
-        aes->stream.vtbl = &mz_stream_aes_vtbl;
+        memset(aes, 0, sizeof(mz_stream_wzaes));
+        aes->stream.vtbl = &mz_stream_wzaes_vtbl;
         aes->encryption_mode = MZ_AES_ENCRYPTION_MODE_256;
     }
     if (stream != NULL)
@@ -363,200 +363,18 @@ void *mz_stream_aes_create(void **stream)
     return aes;
 }
 
-void mz_stream_aes_delete(void **stream)
+void mz_stream_wzaes_delete(void **stream)
 {
-    mz_stream_aes *aes = NULL;
+    mz_stream_wzaes *aes = NULL;
     if (stream == NULL)
         return;
-    aes = (mz_stream_aes *)*stream;
+    aes = (mz_stream_wzaes *)*stream;
     if (aes != NULL)
         MZ_FREE(aes);
     *stream = NULL;
 }
 
-void *mz_stream_aes_get_interface(void)
+void *mz_stream_wzaes_get_interface(void)
 {
-    return (void *)&mz_stream_aes_vtbl;
-}
-
-/***************************************************************************/
-
-static mz_stream_vtbl mz_stream_sha1_vtbl = {
-    mz_stream_sha1_open,
-    mz_stream_sha1_is_open,
-    mz_stream_sha1_read,
-    mz_stream_sha1_write,
-    mz_stream_sha1_tell,
-    mz_stream_sha1_seek,
-    mz_stream_sha1_close,
-    mz_stream_sha1_error,
-    mz_stream_sha1_create,
-    mz_stream_sha1_delete,
-    mz_stream_sha1_get_prop_int64,
-    NULL
-};
-
-/***************************************************************************/
-
-typedef struct mz_stream_sha1_s {
-    mz_stream       stream;
-    int32_t         mode;
-    int32_t         error;
-    int16_t         initialized;
-    int64_t         total_in;
-    int64_t         total_out;
-    sha1_ctx        hash_ctx;
-    uint8_t         hash[SHA1_DIGEST_SIZE];
-} mz_stream_sha1;
-
-/***************************************************************************/
-
-int32_t mz_stream_sha1_open(void *stream, const char *path, int32_t mode)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-
-    sha1->total_in = 0;
-    sha1->total_out = 0;
-    sha1->initialized = 1;
-
-    memset(sha1->hash, 0, sizeof(sha1->hash));
-
-    sha1_begin(&sha1->hash_ctx);
-    return MZ_OK;
-}
-
-int32_t mz_stream_sha1_is_open(void *stream)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    if (sha1->initialized == 0)
-        return MZ_STREAM_ERROR;
-    return MZ_OK;
-}
-
-int32_t mz_stream_sha1_read(void *stream, void *buf, int32_t size)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    int32_t read = size;
-    if (sha1->stream.base)
-        read = mz_stream_read(sha1->stream.base, buf, size);
-    if (read > 0)
-    {
-        sha1_hash(buf, read, &sha1->hash_ctx);
-        sha1->total_in += read;
-    }
-    return read;
-}
-
-int32_t mz_stream_sha1_write(void *stream, const void *buf, int32_t size)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    int32_t written = size;
-
-    if (size < 0)
-        return MZ_PARAM_ERROR;
-    if (sha1->stream.base)
-        written = mz_stream_write(sha1->stream.base, buf, size);
-    if (written > 0)
-    {
-        sha1_hash(buf, written, &sha1->hash_ctx);
-        sha1->total_out += written;
-    }
-    return written;
-}
-
-int64_t mz_stream_sha1_tell(void *stream)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    return mz_stream_tell(sha1->stream.base);
-}
-
-int32_t mz_stream_sha1_seek(void *stream, int64_t offset, int32_t origin)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    return mz_stream_seek(sha1->stream.base, offset, origin);
-}
-
-int32_t mz_stream_sha1_close(void *stream)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-
-    sha1_end(sha1->hash, &sha1->hash_ctx);
-    sha1->initialized = 0;
-    return MZ_OK;
-}
-
-int32_t mz_stream_sha1_error(void *stream)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    return sha1->error;
-}
-
-int32_t mz_stream_sha1_get_digest(void *stream, uint8_t *digest, int32_t digest_size)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    int32_t copy = digest_size;
-    if (digest == NULL || copy < SHA1_DIGEST_SIZE)
-        return MZ_PARAM_ERROR;
-    if (copy > SHA1_DIGEST_SIZE)
-        copy = SHA1_DIGEST_SIZE;
-    memcpy(digest, sha1->hash, copy);
-    return MZ_OK;
-}
-
-int32_t mz_stream_sha1_get_digest_size(void *stream, int32_t *digest_size)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    if (digest_size == NULL)
-        return MZ_PARAM_ERROR;
-    *digest_size = SHA1_DIGEST_SIZE;
-    return MZ_OK;
-}
-
-int32_t mz_stream_sha1_get_prop_int64(void *stream, int32_t prop, int64_t *value)
-{
-    mz_stream_sha1 *sha1 = (mz_stream_sha1 *)stream;
-    switch (prop)
-    {
-    case MZ_STREAM_PROP_TOTAL_IN:
-        *value = sha1->total_in;
-        break;
-    case MZ_STREAM_PROP_TOTAL_OUT:
-        *value = sha1->total_out;
-        break;
-    default:
-        return MZ_EXIST_ERROR;
-    }
-    return MZ_OK;
-}
-
-void *mz_stream_sha1_create(void **stream)
-{
-    mz_stream_sha1 *sha1 = NULL;
-
-    sha1 = (mz_stream_sha1 *)MZ_ALLOC(sizeof(mz_stream_sha1));
-    if (sha1 != NULL)
-    {
-        memset(sha1, 0, sizeof(mz_stream_sha1));
-        sha1->stream.vtbl = &mz_stream_sha1_vtbl;
-    }
-    if (stream != NULL)
-        *stream = sha1;
-
-    return sha1;
-}
-
-void mz_stream_sha1_delete(void **stream)
-{
-    mz_stream_sha1 *sha1 = NULL;
-    if (stream == NULL)
-        return;
-    sha1 = (mz_stream_sha1 *)*stream;
-    if (sha1 != NULL)
-        MZ_FREE(sha1);
-    *stream = NULL;
-}
-
-void *mz_stream_sha1_get_interface(void)
-{
-    return (void *)&mz_stream_sha1_vtbl;
+    return (void *)&mz_stream_wzaes_vtbl;
 }
