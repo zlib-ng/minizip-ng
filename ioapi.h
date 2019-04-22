db31bbd1df061dd202ef54cd18041a23a53af656
Nathan Moinvaziri <nathan@nathanm.com>
2019-04-21 21:49:39 -0700

    Simplify check for file function availability. #380
    An project that requires 32-bit version of file functions should define USE_FILE32API.

--- ioapi.h
+++ ioapi.h
@@ -33,12 +33,7 @@
 #  define ftello64 ftell
 #  define fseeko64 fseek
 #else
-#  if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__ANDROID__)
-#    define fopen64 fopen
-#    define ftello64 ftello
-#    define fseeko64 fseeko
-#  endif
-#  ifdef _MSC_VER
+#  if defined(_MSC_VER)
 #    define fopen64 fopen
 #    if (_MSC_VER >= 1400) && (!(defined(NO_MSCVER_FILE64_FUNC)))
 #      define ftello64 _ftelli64
@@ -47,6 +42,10 @@
 #      define ftello64 ftell
 #      define fseeko64 fseek
 #    endif
+#  else
+#    define fopen64 fopen
+#    define ftello64 ftello
+#    define fseeko64 fseeko
 #  endif
 #endif
 
