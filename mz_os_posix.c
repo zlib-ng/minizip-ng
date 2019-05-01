8193682eac32328b4dddacd39c98a4d1260adc9c
Nathan Moinvaziri <nathan@nathanm.com>
2019-05-01 09:12:48 -0700

    Use memcpy instead of strncpy to avoid gcc 9 warning. #385

--- mz_os_posix.c
+++ mz_os_posix.c
@@ -171,7 +171,7 @@ int32_t mz_os_get_file_date(const char *path, time_t *modified_date, time_t *acc
         /* Not all systems allow stat'ing a file with / appended */
         len = strlen(path);
         name = (char *)malloc(len + 1);
-        strncpy(name, path, len);
+        memcpy(name, path, len);
         name[len] = 0;
 
         if (name[len - 1] == '/' || name[len - 1] == '\\')
