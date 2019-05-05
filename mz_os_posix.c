02eb8cda228a924c2e1bf9765ad2061c5eb917ab
Nathan Moinvaziri <nathan@nathanm.com>
2019-05-05 09:28:49 -0700

    Fixed casting warning.

--- mz_os_posix.c
+++ mz_os_posix.c
@@ -299,7 +299,8 @@ int32_t mz_os_read_symlink(const char *path, char *target_path, int32_t max_targ
 {
     size_t length = 0;
 
-    if ((length = readlink(path, target_path, max_target_path - 1)) == (size_t)-1)
+    length = (size_t)readlink(path, target_path, max_target_path - 1);
+    if (length == (size_t)-1)
         return MZ_EXIST_ERROR;
 
     target_path[length] = 0;
