0a0658d0783ad73ef04a3811711afa1d8c6741a7
Jaehong Kang <j.kang@ridi.com>
2020-01-08 19:03:51 +0900

    Change name to CMinizip

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "minizip",
-            targets: ["minizip"]),
+            targets: ["CMinizip"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -60,7 +60,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "minizip",
+            name: "CMinizip",
             dependencies: [],
             path: ".",
             sources: minizipSources,
