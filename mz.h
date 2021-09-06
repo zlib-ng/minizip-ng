99d39015e29703af2612277180ea586805f655ea
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2021-09-06 12:44:00 -0700

    Version 3.0.3.

--- mz.h
+++ mz.h
@@ -14,8 +14,8 @@
 /***************************************************************************/
 
 /* MZ_VERSION */
-#define MZ_VERSION                      ("3.0.2")
-#define MZ_VERSION_BUILD                (030002)
+#define MZ_VERSION                      ("3.0.3")
+#define MZ_VERSION_BUILD                (030003)
 
 /* MZ_ERROR */
 #define MZ_OK                           (0)  /* zlib */
