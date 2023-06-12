bed38ca132bb049825680e900ca156b979b0df9f
pmqs <pmqs@cpan.org>
2023-06-12 13:46:23 +0100

    Fix memory leak in mz_zip_writer_copy_from_reader https://github.com/zlib-ng/minizip-ng/issues/717

--- mz_zip_rw.c
+++ mz_zip_rw.c
@@ -1737,6 +1737,8 @@ int32_t mz_zip_writer_copy_from_reader(void *handle, void *reader) {
 
         err = mz_zip_writer_entry_open(writer, file_info);
 
+        mz_crypt_sha_delete(&writer->hash);
+
         if ((err == MZ_OK) &&
             (mz_zip_attrib_is_dir(writer->file_info.external_fa, writer->file_info.version_madeby) != MZ_OK)) {
             err = mz_zip_writer_add(writer, reader_zip_handle, mz_zip_entry_read);
