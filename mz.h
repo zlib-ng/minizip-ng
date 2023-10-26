4f670cc7d7ad3a7a27ac3ed5e6c5ddf20956654b
Nathan Moinvaziri <nathan@nathanm.com>
2023-10-26 10:06:19 -0700

    Version 4.0.2.

--- mz.h
+++ mz.h
@@ -14,8 +14,8 @@
 /***************************************************************************/
 
 /* MZ_VERSION */
-#define MZ_VERSION                      ("4.0.1")
-#define MZ_VERSION_BUILD                (040001)
+#define MZ_VERSION                      ("4.0.2")
+#define MZ_VERSION_BUILD                (040002)
 
 /* MZ_ERROR */
 #define MZ_OK                           (0)  /* zlib */
