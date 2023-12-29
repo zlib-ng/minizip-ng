13b01a74a44108557e00c78ffac54e4fae7f8c7f
Nathan Moinvaziri <nathan@nathanm.com>
2023-12-29 07:05:05 -0800

    Version 4.0.4.

--- mz.h
+++ mz.h
@@ -14,8 +14,8 @@
 /***************************************************************************/
 
 /* MZ_VERSION */
-#define MZ_VERSION                      ("4.0.3")
-#define MZ_VERSION_BUILD                (040003)
+#define MZ_VERSION                      ("4.0.4")
+#define MZ_VERSION_BUILD                (040004)
 
 /* MZ_ERROR */
 #define MZ_OK                           (0)  /* zlib */
