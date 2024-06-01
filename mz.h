4cec1162109d90020411ff205faa8cb2518e8427
Nathan Moinvaziri <nathan@nathanm.com>
2024-06-01 14:10:39 -0700

    Version 4.0.6.

--- mz.h
+++ mz.h
@@ -14,8 +14,8 @@
 /***************************************************************************/
 
 /* MZ_VERSION */
-#define MZ_VERSION                      ("4.0.5")
-#define MZ_VERSION_BUILD                (0x040005)
+#define MZ_VERSION                      ("4.0.6")
+#define MZ_VERSION_BUILD                (0x040006)
 
 /* MZ_ERROR */
 #define MZ_OK                           (0)  /* zlib */
