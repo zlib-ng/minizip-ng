a456f977616dce851492b294415d3664e5e4ccd9
emrecanbulut <bulutemre@itu.edu.tr>
2025-09-01 17:26:56 +0200

    Finding the correct library based on dynamic or static choice

--- cmake/FindZLIB-NG.cmake
+++ cmake/FindZLIB-NG.cmake
@@ -13,7 +13,16 @@ if(ZLIB-NG_INCLUDE_DIRS)
     endif()
 endif()
 
-find_library(ZLIB-NG_LIBRARY NAMES z-ng libz-ng libz-ng.a zlib-ng zlibstatic-ng)
+set (LIB_NAMES z-ng libz-ng libz-ng.a)
+
+if(BUILD_SHARED_LIBS)
+    list(APPEND LIB_NAMES zlib-ng)	
+else()
+    list(APPEND LIB_NAMES zlibstatic-ng)
+endif()
+
+find_library(ZLIB-NG_LIBRARY NAMES ${LIB_NAMES})
+
 
 set(ZLIB-NG_LIBRARIES ${ZLIB-NG_LIBRARY})
 set(ZLIB-NG_INCLUDE_DIRS ${ZLIB-NG_INCLUDE_DIRS})
